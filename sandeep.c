7e75020789f2195ff32c87bcac3ea1b611c757ca
Sandeep696 <gandepallisandeep477@gmail.com>
2022-09-07 10:34:46 +0530

    i have updated my real name

--- sandeep.c
+++ sandeep.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
 void main(){
-    printf("my name is sandeep..........");
+    printf("my name is sandeep..........\n");
+    printf("hey hey my name is not sandeep, my name is pulihora raja");
 }
